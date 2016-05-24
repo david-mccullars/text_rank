b06a0cbc8c7a62e2dc8888a5f46a9beac44b09b1
David McCullars <david@bloomfire.com>
2016-05-24 10:21:33 -0500

    1.1.6: Fix number tokenizer to accept 2016, added specs

---
+++ spec/text_rank/tokenizer/number_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'TextRank::Tokenizer::Number' do
+
+  %w[
+    453231162
+    453231162.0
+    4
+    453
+    453,231,162
+    453,231,162.17981
+    0
+    0.17412
+    .1
+  ].each do |s|
+    it "should accept #{s.inspect}" do
+      expect(s.scan(TextRank::Tokenizer::Number)).to eq([[s]])
+    end
+  end
+
+  %w[
+    4532311a62
+    453231162.
+    01
+    .
+    1,1234
+  ].each do |s|
+    it "should NOT accept #{s.inspect}" do
+      expect(s.scan(TextRank::Tokenizer::Number)).to_not eq([[s]])
+    end
+  end
+
+end
